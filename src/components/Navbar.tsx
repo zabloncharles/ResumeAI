f9050fdaf6f93e9134b2de10a6c7b19ae94c48dc
Zablon Charles <zablon.charles@gmail.com>
2025-06-02 15:25:21 -0400

    feat: add favicon description meta tag

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -61,11 +61,11 @@ const Navbar = () => {
             >
               <img
                 src={require("../bunny1.png")}
-                alt="NextStepAi Logo"
+                alt="Brightfolio Logo"
                 className="h-8 w-8"
               />
               <span className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-                NextStepAi
+                Brightfolio
               </span>
             </Link>
           </div>
